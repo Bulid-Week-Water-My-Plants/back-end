c64ed6b8eaa0aed39490d67b66450cf97ecb02f5
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-11-16 12:31:21 -0700

    setup tables

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,22 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
-      users.timestamps(false, true)
+    .createTable('plants', (plants) => {
+      plants.increments('plant_id')
+      plants.string('nickname', 200).notNullable()
+      plants.string('species', 200).notNullable()
+      plants.string('h2oFrequency', 200).notNullable()
+      plants.string('image', 200).notNullable()
     })
+    createTable('users', users => {
+      users.increments();
+      users.string('username', 255).notNullable().unique();
+      users.string('password', 255).notNullable();
+    });
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+  .dropTableIfExists('users')
+  .dropTableIfExists('plant')
+
 }
