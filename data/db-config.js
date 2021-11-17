4e4047671563443223b9dd6228865e07c81e5824
Mitchell Walker <eagleye1144@gmail.com>
2021-11-17 16:57:35 -0700

    server bug fixed

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,6 @@
 
 const knex = require('knex')
-const configurations = require('./../../knexfile')
+const configurations = require('../knexfile')
 const environment = process.env.NODE_ENV || 'development';
 
-module.exports = knex(configs[process.env.NODE_ENV])
+module.exports = knex(configurations[environment])
