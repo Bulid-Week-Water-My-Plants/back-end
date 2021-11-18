24f0683277e113c9e25ee96a690d438fb400fac6
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-11-17 17:02:16 -0700

    server needs changes

--- api/server.js
+++ api/server.js
@@ -6,13 +6,7 @@ const authRouter = require('./auth/auth-router')
 const plantRouter = require('./plants/plants-router')
 function getAllUsers() { return db('users') }
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+
 
 const server = express()
 server.use(express.json())
@@ -25,7 +19,13 @@ server.use('/api/plants', plantRouter)
 server.get('/api/users', async (req, res) => {
   res.json(await getAllUsers())
 })
-
+async function insertUser(user) {
+  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
+  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+}
 server.post('/api/users', async (req, res) => {
   res.status(201).json(await insertUser(req.body))
 })
