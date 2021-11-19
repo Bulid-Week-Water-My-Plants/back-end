ee293c674c0b62a180c4ac5e9bdd37e98e0433de
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-11-19 08:13:01 -0700

    completed all the endpoints

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -1,19 +1,24 @@
 const db = require('../../data/db-config')
 
 async function getPlants(){
-    const plants = await db('users')
+    const plants = await db('plants')
     return plants
 }
 
 async function getPlant(id){
-const plant = await db("plants").where('plants_id', id)
+const plant = await db('plants').where('plant_id', id).select('nickname','h2oFrequency','species', 'image')
 return plant
 }
 
 async function insertPlanet(newPlant){
-    const [plant_id] = await db('plants').insert(newPlant)
-    return getPlant().where({plant_id}).first();
-}
+    const newRecords = await db('plants')
+      .insert(newPlant)
+      .returning(["plant_id"])
+//     const plant_id = newRecords[0].plant_id
+//     return getPlant()
+//       .where({plant_id})
+//       .first();
+ }
 
 function deletePlant(plant_id){
     return db('plants').where({plant_id}).del()
